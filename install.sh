d593d3e4b8e9ac1d01e154c76980b23d7e02a348
Yuri Toledo <yuriwtoledo@gmail.com>
2019-03-04 14:05:30 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -14,11 +14,5 @@ yay -S albert-lite gpick papirus-folder-git papirus-icon-theme
 echo 'Installing anothoer stuffs ...'
 yay -S spotify-stable gpick papirus-folder-git sassc noto-fonts-emoji xclip
 
-
-# plata-theme st
-./autogen.sh --prefix=/usr --with-selection_color="#009688" --with-accent_color="#4DB6AC" --with-suggestion_color="#00BCD4" --disable-xfce --disable-mate --disable-flashback  --disable-openbox --disable-gnome
-
-
-
 echo 'Limpando tudo ...'
 yay -Yc
