6a0ab722a50e9b05ff88ac94195b3a3f2f9fc6a7
Yuri Toledo <yuriwtoledo@gmail.com>
2019-03-06 08:33:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,15 +9,15 @@ echo 'Installing dev stuffs...'
 yay -S visual-studio-code-bin sublime-text-dev yarn nodejs insomnia android-sdk android-sdk-platform-tools genymotion virtual-box-ext-oracle
  
 echo 'Installing desktop stuffs ...'
-yay -S albert-lite gpick papirus-folder-git papirus-icon-theme
+yay -S albert-lite papirus-folder-git papirus-icon-theme
 
-echo 'Installing anothoer stuffs ...'
+echo 'Installing another stuffs ...'
 yay -S spotify-stable gpick papirus-folder-git sassc noto-fonts-emoji xclip gpaste-deamon google-chrome alarm-clock-applet jdk8 
 
 
 
 
-echo 'Limpando tudo ...'
+echo 'Cleaning all ...'
 yay -Yc
 
 
@@ -31,4 +31,4 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 
-echo 'Deixar as fontes em tamanho 10.5 e a title, 11.25. Para monospace, fira code retina 10.5'
+echo 'Fonts: 10.5 |  window title: 11.25 | fira code retina 10.5'
